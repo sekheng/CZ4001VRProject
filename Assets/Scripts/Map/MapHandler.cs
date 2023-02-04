0915f550545eb974bc06f5e3b2b0294c7b45f031
sekheng <leesekheng@yahoo.com>
2023-02-04 17:57:52 +0800

    Roughly how the map looks like in a prototype

--- Assets/Scripts/Map/MapHandler.cs
+++ Assets/Scripts/Map/MapHandler.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class MapHandler : MonoBehaviour
 {
+    [Header("For Debugging")]
+    [SerializeField, Tooltip("Grid Range from (0,0,0)")]
+    Vector2 m_GridRange = new Vector2(200, 200);
+    [SerializeField, Tooltip("Gizmos Grid Size")]
+    Vector2 m_GizmosGridSize = new Vector2(10,10);
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// To draw the rough grid size then 
+    /// </summary>
+    private void OnDrawGizmos()
     {
-        
+        // based on the gizmos
+        for (float x = m_GridRange.x; x >= -m_GridRange.x; x -= m_GizmosGridSize.x)
+        {
+            for (float y = m_GridRange.y; y >= -m_GridRange.y; y -= m_GizmosGridSize.y)
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawWireCube(new Vector3(x, 0, y), new Vector3(m_GizmosGridSize.x, 1, m_GizmosGridSize.y));
+            }
+        }
     }
 }
