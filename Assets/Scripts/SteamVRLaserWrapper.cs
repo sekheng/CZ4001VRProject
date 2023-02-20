bf4adeaccafb64f66b27c1259e9f551b7a15fe12
Ryan Phuay <57478357+Noliferist@users.noreply.github.com>
2023-02-21 04:02:30 +0800

    LaserPointer and Wrapper (Not working yet)

---
+++ Assets/Scripts/SteamVRLaserWrapper.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Valve.VR.Extras;
+
+ public class SteamVRLaserWrapper : MonoBehaviour
+ {
+    public SteamVR_LaserPointer laserPointer;
+
+    void Awake()
+    {
+        laserPointer.PointerIn += PointerInside;
+        laserPointer.PointerOut += PointerOutside;
+        laserPointer.PointerClick += PointerClick;
+    }
+
+    public void PointerClick(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "Start Game")
+        {
+            Debug.Log("Start Game was clicked");
+        } else if (e.target.name == "Quit Button")
+        {
+            Debug.Log("Quit Button was clicked");
+        }
+    }
+
+    public void PointerInside(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "Start Game")
+        {
+            Debug.Log("Start Game was entered");
+        }
+        else if (e.target.name == "Quit Button")
+        {
+            Debug.Log("Quit Button was entered");
+        }
+    }
+
+    public void PointerOutside(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "Start Game")
+        {
+            Debug.Log("Start Game was exited");
+        }
+        else if (e.target.name == "Quit Button")
+        {
+            Debug.Log("Quit Button was exited");
+        }
+    }
+}
